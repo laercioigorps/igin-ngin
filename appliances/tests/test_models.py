0eff78528aa42ca3f39ff8d12ff890f6b9d28cd3
laercio <laercioigorps@gmail.com>
2021-12-12 14:20:19 -0300

    added test for category class

--- appliances/tests/test_models.py
+++ appliances/tests/test_models.py
@@ -1,9 +1,18 @@
 from django.test import TestCase
-from appliances.models import Brand
+from appliances.models import Brand, Category
 
 # Create your tests here.
+
+
 class BrandTest(TestCase):
     def test_create_brand(self):
         Brand.objects.create(name="Brastemp")
         myBrand = Brand.objects.get(name="Brastemp")
         self.assertEquals(myBrand.name, "Brastemp")
+
+
+class CategoryTest(TestCase):
+    def test_create_brand(self):
+        Category.objects.create(name="Geladeira")
+        category = Category.objects.get(name="Geladeira")
+        self.assertEquals(category.name, "Geladeira")
