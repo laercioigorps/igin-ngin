45b73015b55e0a654bb1cc490a349ee4c38212fd
laercio <laercioigorps@gmail.com>
2021-12-12 14:39:23 -0300

    added Appliance model

--- appliances/models.py
+++ appliances/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
 
@@ -9,3 +10,9 @@ class Brand(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+
+
+class Appliance(models.Model):
+    model = models.CharField(max_length=20)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
