ca5bb561a79a3bb63294e77fe6b721814e708244
laercio <laercioigorps@gmail.com>
2021-12-12 16:25:06 -0300

    added brand_list_view that accepts post request to create brand

--- appliances/views.py
+++ appliances/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from appliances.serializers import BrandSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.parsers import JSONParser
 
 # Create your views here.
+
+
+@api_view(['POST'])
+def brand_list_view(request, format=None):
+    if(request.method == 'POST'):
+        data = JSONParser().parse(request)
+        serializer = BrandSerializer(data=data)
+        if(serializer.is_valid()):
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+    return Response(status=400)
