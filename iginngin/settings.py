2b2a15f3ec22ca6d05a91c3a1d8fe662cfffdff7
laercio <laercioigorps@gmail.com>
2021-12-12 06:53:32 -0300

    added appliances app to installed apps

--- iginngin/settings.py
+++ iginngin/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'appliances.apps.AppliancesConfig',
     'rest_framework',
 ]
 
