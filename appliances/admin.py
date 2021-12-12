24ff1dcca4dfb524becc18bce80594ee39dcfa86
laercio <laercioigorps@gmail.com>
2021-12-12 14:46:46 -0300

    registers Appliance, Brand and Category models

--- appliances/admin.py
+++ appliances/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from appliances.models import Appliance, Brand, Category
 
 # Register your models here.
+
+admin.site.register(Appliance)
+admin.site.register(Brand)
+admin.site.register(Category)
