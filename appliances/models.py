da79b28313a4352049d07e1b47ae929500fc78d5
laercio <laercioigorps@gmail.com>
2021-12-12 14:20:36 -0300

    Category model added

--- appliances/models.py
+++ appliances/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Brand(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Category(models.Model):
     name = models.CharField(max_length=50)
