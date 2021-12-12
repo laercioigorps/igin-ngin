b47d1d8497b1f480e0acacc8969767c1f2e91d47
laercio <laercioigorps@gmail.com>
2021-12-12 14:38:55 -0300

    added Appliance basic tests

--- appliances/tests/test_models.py
+++ appliances/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from appliances.models import Brand, Category
+from appliances.models import Brand, Category, Appliance
 
 # Create your tests here.
 
@@ -12,7 +12,22 @@ class BrandTest(TestCase):
 
 
 class CategoryTest(TestCase):
-    def test_create_brand(self):
+    def test_create_category(self):
         Category.objects.create(name="Geladeira")
         category = Category.objects.get(name="Geladeira")
         self.assertEquals(category.name, "Geladeira")
+
+
+class ApplianceTest(TestCase):
+
+    def setUp(self):
+        self.brand1 = Brand.objects.create(name="Brastemp")
+        self.category1 = Category.objects.create(name="Geladeira")
+
+    def test_create_Apliance(self):
+        Appliance.objects.create(
+            model="BWC11ABANA", brand=self.brand1, category=self.category1)
+        appliance = Appliance.objects.get(model="BWC11ABANA")
+        self.assertEquals(appliance.model, "BWC11ABANA")
+        self.assertEquals(appliance.brand.name, "Brastemp")
+        self.assertEquals(appliance.category.name, "Geladeira")
