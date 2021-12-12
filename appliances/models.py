4e634324b222be6c1c4eb03e096b9eb39dbe028d
laercio <laercioigorps@gmail.com>
2021-12-12 14:47:21 -0300

    update models to use __str__ method

--- appliances/models.py
+++ appliances/models.py
@@ -7,12 +7,21 @@ from django.db.models.fields.related import ForeignKey
 class Brand(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Appliance(models.Model):
     model = models.CharField(max_length=20)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.model
