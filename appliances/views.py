48ab9f87a8efaa18506e8fdfa0a4f542cec78683
laercio <laercioigorps@gmail.com>
2021-12-18 06:10:54 -0300

    implement get method in brand_list

--- appliances/views.py
+++ appliances/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from appliances.models import Brand
 from appliances.serializers import BrandSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
@@ -9,9 +10,13 @@ from rest_framework.parsers import JSONParser
 # Create your views here.
 
 
-@api_view(['POST'])
+@api_view(['POST', 'GET'])
 @permission_classes([permissions.IsAuthenticated])
 def brand_list_view(request, format=None):
+    if(request.method == 'GET'):
+        brands = Brand.objects.all()
+        serializer = BrandSerializer(brands, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     if(request.method == 'POST'):
         data = JSONParser().parse(request)
         serializer = BrandSerializer(data=data)
