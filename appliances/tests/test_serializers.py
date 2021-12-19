3f115db4592f4c0a78a3c9bab5f6eab1b23f67c0
laercio <laercioigorps@gmail.com>
2021-12-19 06:14:35 -0300

    test CategorySerializer

--- appliances/tests/test_serializers.py
+++ appliances/tests/test_serializers.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
-from appliances.models import Brand
-from appliances.serializers import BrandSerializer
+from appliances.models import Brand, Category
+from appliances.serializers import BrandSerializer, CategorySerializer
 import io
 
 
@@ -38,3 +38,30 @@ class BrandSerializerTest(TestCase):
         getBrand = Brand.objects.get(name='Continental')
 
         self.assertEqual(getBrand.name, 'Continental')
+
+
+class CategorySerializerTest(TestCase):
+
+    def setUp(self):
+        self.category1 = Category.objects.create(name="Geladeira")
+        self.category2 = Category.objects.create(name="Freezer")
+        self.category3 = Category.objects.create(name="Maquina de lavar")
+
+    def test_category_serializer_create(self):
+        count = Category.objects.all().count()
+        self.assertEqual(count, 3)
+
+        category = Category(name='Microondas')
+        serializer = CategorySerializer(category)
+        data = get_json_data(serializer.data)
+
+        serializer = CategorySerializer(data=data)
+        serializer.is_valid()
+        serializer.save()
+
+        count = Category.objects.all().count()
+        self.assertEqual(count, 4)
+
+        getCategory = Category.objects.get(pk=4)
+
+        self.assertEqual(getCategory.name, 'Microondas')
