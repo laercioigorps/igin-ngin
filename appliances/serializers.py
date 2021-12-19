073919009b981a9a7505571e5ba5165648d67c94
laercio <laercioigorps@gmail.com>
2021-12-19 06:14:58 -0300

    added CategorySerializer modelSerializer

--- appliances/serializers.py
+++ appliances/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
-from .models import Brand
+from .models import Brand, Category
 
 
 class BrandSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Brand
+        fields = ['name']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
         fields = ['name']
