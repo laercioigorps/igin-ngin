dc81a0c502fd9dd17206012b77611c603c9deb7f
laercio <laercioigorps@gmail.com>
2021-12-19 06:17:41 -0300

    added path categories named category_list pointing to views.list_view

--- appliances/urls.py
+++ appliances/urls.py
@@ -6,5 +6,6 @@ app_name = 'appliances'
 
 urlpatterns = [
     path('brands/', views.brand_list_view, name="brand_list"),
-    path('brands/<int:pk>/', views.brand_detail_view, name="brand_detail")
+    path('brands/<int:pk>/', views.brand_detail_view, name="brand_detail"),
+    path('categories/', views.category_list_view, name="category_list")
 ]
