34ccb1f2c14218f2c15f572dbd1c2d459e5b25ba
laercio <laercioigorps@gmail.com>
2021-12-25 10:00:02 -0300

    added profiles in installed_apps

--- iginngin/settings.py
+++ iginngin/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'appliances.apps.AppliancesConfig',
+    'profiles.apps.ProfilesConfig',
     'rest_framework',
 ]
 
